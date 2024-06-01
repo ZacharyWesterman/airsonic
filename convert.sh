b8382b8a2cdaa05d5a4b0281d515b881c1c37f3c
Zachary Westerman <westerman.zachary@gmail.com>
2024-06-01 16:33:40 -0500

    Improve conversion script

--- convert.sh
+++ convert.sh
@@ -8,16 +8,49 @@ src/require.sh || exit 1
 
 cd /home/airsonic || exit 1
 
-while read m4a
-do
-	mp3="${m4a%.m4a*}.mp3"
-	</dev/null ffmpeg -i "$m4a" "$mp3" -y -loglevel error -nostats && rm "$m4a"
-	echo "$mp3"
-done < <(find . -type f -name '*.m4a')
+#Convert a given file to mp3
+convert_file()
+{
+	local src
+	local mp3
+	local total
+	local current
 
-while read m4b
+	src="$1"
+	mp3="${src%.*}.mp3"
+
+	current="$2"
+	total="$3"
+
+	echo "[$current/$total] Converting: $(basename "$mp3")"
+
+	ffmpeg -i "$src" "$mp3" -y -loglevel error -nostats </dev/null >/dev/null && rm "$src"
+}
+
+MAXPROC=$(nproc)
+MAXPROC=$((MAXPROC - 1))
+
+echo -n 'Counting files...'
+TOTAL=0
+for folder in Audiobooks Music Soundtracks
 do
-	mp3="${m4b%.m4b*}.mp3"
-	</dev/null ffmpeg -i "$m4b" "$mp3" -y -loglevel error -nostats && rm "$m4b"
-	echo "$mp3"
-done < <(find . -type f -name '*.m4b')
+	ct="$(find "$folder/" -type f \( -name '*.m4a' -or -name '*.m4b' \) | wc -l)"
+	TOTAL=$((TOTAL + ct))
+done
+echo " $TOTAL files will be converted."
+
+CURRENT=0
+for folder in Audiobooks Music Soundtracks
+do
+	ct=0
+	while read src
+	do
+		CURRENT=$((CURRENT + 1))
+		ct=$((ct+1))
+		[ "$ct" -gt "$MAXPROC" ] && ct=0 && wait
+		convert_file "$src" "$CURRENT" "$TOTAL" &
+	done < <(find "$folder/" -type f \( -name '*.m4a' -or -name '*.m4b' \))
+	wait
+done
+
+echo "All $TOTAL files have been converted."

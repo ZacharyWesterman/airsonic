ec5d63e717dfe8f0cc6922df819f62dddf822ec6
Zachary Westerman <westerman.zachary@gmail.com>
2024-07-03 14:40:10 -0500

    don't wait on ALL batched convs to finish; start next conv as soon as there's room for it

--- convert.sh
+++ convert.sh
@@ -1,12 +1,29 @@
 #!/bin/bash
 
+if [ "$#" -lt 1 ]
+then
+	>&2 echo "ERROR: No directories specified. Please specify at least one directory to convert."
+	exit 1
+fi
+
+for folder in "$@"
+do
+	if [ ! -d "$folder" ]
+	then
+		>&2 echo "ERROR: \"$folder\" is not a directory!"
+		exit 1
+	fi
+done
+
+cwd=$(pwd)
+
 #make sure we're in this script's working dir
 cd "$(dirname "${BASH_SOURCE[0]}")" || exit 1
 
 #check dependencies and exit if any are not met
 src/require.sh || exit 1
 
-cd /home/airsonic || exit 1
+cd "$cwd" || exit 1
 
 #Convert a given file to mp3
 convert_file()
@@ -24,7 +41,7 @@ convert_file()
 
 	echo "[$current/$total] Converting: $(basename "$mp3")"
 
-	ffmpeg -i "$src" "$mp3" -y -loglevel error -nostats </dev/null >/dev/null && rm "$src"
+	ffmpeg -i "$src" "$mp3" -y -loglevel error -nostats </dev/null >/dev/null #&& rm "$src"
 }
 
 MAXPROC=$(nproc)
@@ -32,7 +49,7 @@ MAXPROC=$((MAXPROC - 1))
 
 echo -n 'Counting files...'
 TOTAL=0
-for folder in Audiobooks Music Soundtracks
+for folder in "$@"
 do
 	ct="$(find "$folder/" -type f \( -name '*.m4a' -or -name '*.m4b' \) | wc -l)"
 	TOTAL=$((TOTAL + ct))
@@ -40,17 +57,39 @@ done
 echo " $TOTAL files will be converted."
 
 CURRENT=0
-for folder in Audiobooks Music Soundtracks
+PIDS=()
+for folder in "$@"
 do
-	ct=0
 	while read src
 	do
+		[ ! -e "$src" ] && continue
+
 		CURRENT=$((CURRENT + 1))
-		ct=$((ct+1))
-		[ "$ct" -gt "$MAXPROC" ] && ct=0 && wait
+
+		while [ "${#PIDS[@]}" -gt "$MAXPROC" ]
+		do
+			#Remove any pids that are finished
+			pids=("${PIDS[@]}")
+			PIDS=()
+			for pid in "${pids[@]}"
+			do
+				if [[ ! -e /proc/"$pid" ]]
+				then
+					wait "$pid"
+				else
+					PIDS+=("$pid")
+				fi
+			done
+
+			sleep 1
+		done
+
 		convert_file "$src" "$CURRENT" "$TOTAL" &
+		PIDS+=($!)
+		sleep .1
 	done < <(find "$folder/" -type f \( -name '*.m4a' -or -name '*.m4b' \))
-	wait
 done
+
+wait
 
 echo "All $TOTAL files have been converted."
